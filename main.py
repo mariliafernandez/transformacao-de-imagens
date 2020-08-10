9ba90305300b830c5b568c8a9cbffb4ae20b81bf
mariliafernandez <marilia.mfernandez@gmail.com>
2020-08-10 16:29:26 -0300

    Função para inverter os valores de intensidade

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import argparse
 import cv2 as cv
 import sys
 
+# Inverte os valores de intensidade da imagem
 def invert(img):
-    print(1)
+    for row in img:
+        for pixel in row:
+            intensisy = pixel[0]
+            pixel[:] = 255 - intensisy
+    cv.imshow('Inverted', img)
+    cv.waitKey(0)
+    cv.imwrite('inverted.jpg', img)
 
 def switch_columns(img):
     print(2)
@@ -14,23 +21,21 @@ def switch_rows(img):
 def histogram_stretching(img):
     print(4)
     
-
+# Lê os argumentos passados via terminal
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', help='operation', type=int)
 parser.add_argument('-i', help='image')
-
 args = parser.parse_args()
-
 operation = args.t
 image = args.i
 
+# Abre a imagem em escala de cinza
 img = cv.imread(image)
+if img is None : sys.exit('Could not read the image.')
+else: gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-if img is None:
-    sys.exit('Could not read the image.')
-
-if operation == 1: invert(img)
-elif operation == 2: switch_columns(img)
-elif operation == 3: switch_rows(img)
-elif operation == 4: histogram_stretching(img)
+if operation == 1: invert(gray)
+elif operation == 2: switch_columns(gray)
+elif operation == 3: switch_rows(gray)
+elif operation == 4: histogram_stretching(gray)
 else: print('Invalid option')
