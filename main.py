5e5e3fe19876bba846cb14bf2b8e384d716b787b
mariliafernandez <marilia.mfernandez@gmail.com>
2020-08-11 16:24:25 -0300

    Função para plotar histograma da figura original e após aplicação do histogram_stretching

--- main.py
+++ main.py
@@ -37,27 +37,48 @@ def switch_rows(img):
 def histogram_stretching(img):
     max_int = np.max(img)
     min_int = np.min(img)
+
     return int(255/(max_int-min_int))*(img-min_int)
     
-# Plota o histograma (distribuição de instensidade da imagem)
-def print_histogram(img):
-    max_int = np.max(img)
-    min_int = np.min(img)
-    frequency = {}
 
-    for i in range(min_int, max_int):
-        frequency[i] = np.sum( img == i ) 
+# Plota o histograma (distribuição de instensidade) da imagem original e depois do stretching
+def plot_histogram(original, stretched):
+    # Cria uma figura dividida em duas partes com eixos compartilhados
+    fig, axs = plt.subplots( 2, sharex=True, sharey=True )
+    fig.suptitle('Histograma')
+
+    # Cria um dicionário das frequências de intensidade da imagem original
+    max1 = np.max(original)
+    min1 = np.min(original)
+    f1 = {}
+    for i in range( min1, max1):
+        f1[i] = np.sum( original == i )
+    axis1 = f1.items()     
+    x1, y1 = zip( *axis1 )
+
+    # Cria um dicionário das frequências de intensidade da imagem após o histogram stretching
+    max2 = np.max(stretched)
+    min2 = np.min(stretched)
+    f2 = {}
+    for i in range( min2, max2):
+        f2[i] = np.sum( stretched == i )
+    axis2 = f2.items()  
+    x2, y2 = zip( *axis2 )
     
-    axis = sorted( frequency.items() )
-    x, y = zip(*axis)
-    plt.bar(x, y)
-    plt.title('Histograma')
+    # Plota as duas frequênciasem gráfico de barras
+    axs[0].bar( x1,y1 ) 
+    axs[0].set_title('Original')
+
+    axs[1].bar( x2,y2 )
+    axs[1].set_title('Stretched')
+
     plt.show()
+
 
 # Interpreta os argumentos passados via terminal
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', help='operation [1, 2, 3, 4]', type=int)
-parser.add_argument('-i', help='image')
+parser.add_argument('-i', help='image in png format')
 args = parser.parse_args()
 operation = args.t
 image = args.i
@@ -68,7 +89,7 @@ if img_file is None : print('Não foi possível ler a imagem.')
 else: 
     # Converte as cores originais da imagem para tons de cinza e salva a nova imagem em arquivo
     gray = cv.cvtColor(img_file, cv.COLOR_BGR2GRAY)
-    cv.imwrite('input_tons_de_cinza.jpg', gray)
+    cv.imwrite('input_tons_de_cinza.png', gray)
 
     # Escolhe a opção de transformação
     if operation == 1: 
@@ -79,9 +100,10 @@ else:
         result = switch_rows(gray)
     elif operation == 4: 
         result = histogram_stretching(gray)
+        plot_histogram(gray, result)
     else: print('Invalid option')
 
     # Mostra o resultado e escreve em arquivo
     cv.imshow('Transformação', result)
     cv.waitKey(0)
-    cv.imwrite('output.jpg', result)
+    cv.imwrite('output.png', result)
